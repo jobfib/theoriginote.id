f45df891a8a87bd7f2f2b64ae23807b9336c76dc
Purwanto <purwanto1337@gmail.com>
2023-11-06 21:36:12 +0700

    fix

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,11 @@
 <template>
   <div>
+    <nav>
+      <NuxtLink to="/" class="bg-red-400 py-2 px-3 text-white">Home</NuxtLink> |
+      <NuxtLink to="/about" class="bg-red-400 py-2 px-3 text-white"
+        >About</NuxtLink
+      >
+    </nav>
     <slot></slot>
   </div>
 </template>
