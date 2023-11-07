f1f6d29fe940e2978fde207fc870e9c1b00b4b9d
Purwanto <purwanto1337@gmail.com>
2023-11-07 13:28:46 +0700

    Updates

--- _nuxt/about.e857b200.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c as o,o as t,a as c}from"./entry.d9615d42.js";const s={},n=c("h1",{class:"underline"},"About v.2",-1),a=[n];function r(_,d){return t(),o("div",null,a)}const i=e(s,[["render",r]]);export{i as default};
